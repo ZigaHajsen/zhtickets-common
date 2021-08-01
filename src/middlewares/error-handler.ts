e672be7a26fc9d648243d82fb644c3d8a72cda35
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-08-01 20:12:26 +0200

    Updates

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+
+  const defaultError = [{ message: 'Something went wrong' }];
+
+  res.status(500).send({
+    errors: defaultError,
+  });
+};
